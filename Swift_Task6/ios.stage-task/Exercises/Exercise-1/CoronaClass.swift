6e2f85d45d101d1912fc3b95498f5b8cc0f63369
Daniil Melnik <daniilmelnik@inbox.ru>
2021-06-21 20:49:37 +0300

    feat: i tried

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -1,18 +1,25 @@
 import Foundation
 
 class CoronaClass {
- 
-     var seats = [Int]()
-
+    var seats = [Int]()
+    var countOfSeats = 0
      init(n: Int) {
- 
+       countOfSeats = n
      }
-     
+   
      func seat() -> Int {
-        -1
+        if seats.isEmpty {
+            seats.append(0)
+        } else {
+            seats.append(countOfSeats-1)
+        }
+        return 0
      }
      
      func leave(_ p: Int) {
-         
+        if seats.isEmpty {
+        } else {
+            seats.remove(at:0)
+        }
      }
 }
